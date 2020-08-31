f7650bd9ae9fc2f1743740da4fc8208f8d5a3b04
Aalya <aalya.banu@gmail.com>
2020-08-31 12:53:44 +0100

    make it better

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const express = require('express'); //require methods loads the express module
+const express = require("express"); //require methods loads the express module
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const router=require('./router')
+const router = require("./router");
 
 app.use(express.json());
-app.use(router)
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
